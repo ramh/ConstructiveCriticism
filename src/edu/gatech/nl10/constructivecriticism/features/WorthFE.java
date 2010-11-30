41308d6b7c18a0d7a5fcc9cdad40786072ebc2e2
Kelsey Hawkins <kphawkins@gmail.com>
2010-11-30 12:14:18 -0500

    Add WorthFE.java

---
+++ src/edu/gatech/nl10/constructivecriticism/features/WorthFE.java
@@ -0,0 +1,25 @@
+package edu.gatech.nl10.constructivecriticism.features;
+
+import java.util.ArrayList;
+
+import edu.gatech.nl10.constructivecriticism.models.Comment;
+
+public class WorthFE implements FeatureExtractor{
+	private ArrayList<String> fnames;
+
+	public ArrayList<String> featureNames() {
+
+		fnames = new ArrayList<String>();
+		fnames.add("worth");
+		return fnames;
+	}
+
+	public ArrayList<Double> extractFeatures(Comment c) {
+		ArrayList<Double> features = new ArrayList<Double>();	
+		features.add((double)c.worth);
+	
+		return features;
+	}
+	
+
+}
